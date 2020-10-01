45bbd318cdee8c293a8bcb29ce8bac75c97324a1
Ken Wood <42664574+GreyBacon@users.noreply.github.com>
2020-09-30 23:46:15 -0700

    Added URL support More UI changes and dictonary additions

--- cleanup.py
+++ cleanup.py
@@ -1,17 +1,12 @@
 import re
 
-# Clear hashtags
-'''
-def clearhash(tweet):
-    tweet = re.sub('@.*([ ])', '', tweet)
-    return tweet
-'''
-
 # Holds the dictionary of words to change
 def cleanup(tweet):
     # politics
-    tweet = re.sub(r'(republican|republicans|repubs|GOP|the right)', 'silly willies', tweet, flags=re.IGNORECASE)
-    tweet = re.sub(r'(democrat|democrats|dems|the left|leftist)', 'teletubbies', tweet, flags=re.IGNORECASE)
+    tweet = re.sub(r'(republicans|repubs|GOP|the right)', 'silly willies', tweet, flags=re.IGNORECASE)
+    tweet = re.sub(r'(republican|conservative)', 'clown', tweet, flags=re.IGNORECASE)
+    tweet = re.sub(r'(democrats|dems|the left|leftist)', 'teletubbies', tweet, flags=re.IGNORECASE)
+    tweet = re.sub(r'(democrat|liberal)', 'mime', tweet, flags=re.IGNORECASE)
     tweet = re.sub(r'(\belection|elections|reelection|re-election)', 'big ol party', tweet, flags=re.IGNORECASE)
     tweet = re.sub(r'(president|presidency)', 'cool dude', tweet, flags=re.IGNORECASE)
     tweet = re.sub(r'(\bimpeach\b)', 'hug', tweet, flags=re.IGNORECASE)
@@ -22,9 +17,11 @@ def cleanup(tweet):
     tweet = re.sub(r'(rigged)', 'wonderful', tweet, flags=re.IGNORECASE)
     tweet = re.sub(r'(blm|black lives matter)', 'free icecream for everyone', tweet, flags=re.IGNORECASE)
     tweet = re.sub(r'(white supremacists)', 'pineapple pizza-eaters', tweet, flags=re.IGNORECASE)
-    tweet = re.sub(r'(white supremacy)', 'pineapple pizza-eating', tweet, flags=re.IGNORECASE)
-    tweet = re.sub(r'(corrupt)', 'smelly', tweet, flags=re.IGNORECASE)
+    tweet = re.sub(r'(white supremacist|racist|sexist)', 'pineapple pizza-eater', tweet, flags=re.IGNORECASE)
+    tweet = re.sub(r'(white supremacy|racism|sexism)', 'pineapple pizza-eating', tweet, flags=re.IGNORECASE)
+    tweet = re.sub(r'(\bcorrupt\b)', 'smelly', tweet, flags=re.IGNORECASE)
     tweet = re.sub(r'(corruption)', 'tomfoolery', tweet, flags=re.IGNORECASE)
+    tweet = re.sub(r'(antifa)', 'fairies', tweet, flags=re.IGNORECASE)
 
     tweet = re.sub(r'(trump)', 'Duck', tweet, flags=re.IGNORECASE)
     tweet = re.sub(r'(biden)', 'Bro', tweet, flags=re.IGNORECASE)
@@ -32,9 +29,11 @@ def cleanup(tweet):
 
     tweet = re.sub(r'(coronavirus|covid-19|covid19|\bvirus\b|plague)', 'snuffles', tweet, flags=re.IGNORECASE)
     tweet = re.sub(r'(dead)', 'sleeping', tweet, flags=re.IGNORECASE)
+    tweet = re.sub(r'(deaths)', 'sneezes', tweet, flags=re.IGNORECASE)
     tweet = re.sub(r'(kill)', 'encourage', tweet, flags=re.IGNORECASE)
     tweet = re.sub(r'(terrorists)', 'goblins', tweet, flags=re.IGNORECASE)
     tweet = re.sub(r'(terrorism)', 'candy-stealing', tweet, flags=re.IGNORECASE)
+
 
     # badwords
     tweet = re.sub(r'(idiot|idiots|morons|dumbass|dumbasses|losers)', 'goobers', tweet, flags=re.IGNORECASE)
@@ -42,4 +41,6 @@ def cleanup(tweet):
     tweet = re.sub(r'(\bass\b|asshole)', 'bum', tweet, flags=re.IGNORECASE)
     tweet = re.sub(r'(dick)', 'stick', tweet, flags=re.IGNORECASE)
 
+    # & symbol since it comes in as &amp:
+    tweet = re.sub(r'(&amp;)', '&', tweet, flags=re.IGNORECASE)
     return tweet
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tweepy
 import tkinter as tk
 import twitauth
 from cleanup import cleanup
+import re
 
 # Consumer key and secret referred to in twitauth.py
 # If using this code you will need to make a twitauth.py file with consumer_key and consumer_secret
@@ -21,32 +22,64 @@ def run_tweets(event='', msg_counter=[]):
         reset_counter += 1
     msg_counter.clear()
 
-    # Search all tweets
-    loop_pos = 0
-    for status in tweepy.Cursor(api.search, q=user_input.get() + ' -filter:retweets', tweet_mode='extended',
-                                result_type='mixed').items(100):
-        if loop_pos < 10:
-            # Clean up emoji and other invalid characters
-            valid_text = status.full_text
+    # Catch any errors that might come from the input
+    try:
+        # Check if the entry is a twitter url and post specific tweet if so
+        if re.search('^(https:\/\/twitter\.com\/)', user_input.get()) and re.search('\/(\d+)$', user_input.get()):
+            # Pulls the tweet ID out of the url
+            url = re.search('(\d+)$', user_input.get()).group(0)
+
+            # Output original tweet
+            valid_text = api.get_status(url, tweet_mode="extended").full_text
             valid_text = ''.join(c for c in valid_text if c <= '\uFFFF')
+            orig_message = tk.Message(root, text=valid_text, background='antiquewhite1', width='400')
+            orig_message.grid(column=0, row=3, sticky='W' + 'E')
+            msg_counter.append(orig_message)
 
-            # Check if there were any changes to each post
-            # Alternate colors per line and output original/changed text
-            if cleanup(valid_text) != valid_text:
-                if (loop_pos % 2) == 0:
-                    alternate_colors = 'antiquewhite1'
-                else:
-                    alternate_colors = root.cget('bg')
+            #Post changed tweet only if there were changes
+            if valid_text == cleanup(valid_text):
+                clean_message = tk.Message(root, text="No changes were made", background='antiquewhite1', width='400')
+                clean_message.grid(column=1, row=3, sticky='W' + 'E')
+                msg_counter.append(clean_message)
+            else:
+                clean_message = tk.Message(root, text=cleanup(valid_text), background='antiquewhite1', width='400')
+                clean_message.grid(column=1, row=3, sticky='W' + 'E')
+                msg_counter.append(clean_message)
 
-                # Output original and cleaned up tweets into two columns
-                orig_message = tk.Message(root, text=valid_text, background=alternate_colors, width='400')
+        # Search up to 10 tweets matching inputted term
+        else:
+            loop_pos = 0
+            for status in tweepy.Cursor(api.search, q=user_input.get() + ' -filter:retweets', tweet_mode='extended',
+                                        result_type='mixed').items(100):
+                if loop_pos < 10:
+                    # Clean up emoji and other invalid characters
+                    valid_text = status.full_text
+                    valid_text = ''.join(c for c in valid_text if c <= '\uFFFF')
+
+                    # Check if there were any changes to each post
+                    # Alternate colors per line and output original/changed text
+                    if cleanup(valid_text) != valid_text:
+                        if (loop_pos % 2) == 0:
+                            alternate_colors = 'antiquewhite1'
+                        else:
+                            alternate_colors = root.cget('bg')
+
+                        # Output original and cleaned up tweets into two columns
+                        orig_message = tk.Message(root, text=valid_text, background=alternate_colors, width='400')
+                        orig_message.grid(column=0, row=loop_pos + 3, sticky='W' + 'E')
+                        msg_counter.append(orig_message)
+                        clean_message = tk.Message(root, text=cleanup(valid_text), background=alternate_colors, width='400')
+                        clean_message.grid(column=1, row=loop_pos + 3, sticky='W' + 'E')
+                        msg_counter.append(clean_message)
+                        loop_pos += 1
+            if loop_pos == 0:
+                orig_message = tk.Message(root, text="No fitting messages were found", background='antiquewhite1', width='400')
                 orig_message.grid(column=0, row=loop_pos + 3, sticky='W' + 'E')
                 msg_counter.append(orig_message)
-                clean_message = tk.Message(root, text=cleanup(valid_text), background=alternate_colors, width='400')
-                clean_message.grid(column=1, row=loop_pos + 3, sticky='W' + 'E')
-                msg_counter.append(clean_message)
-                loop_pos += 1
-
+    except:
+        clean_message = tk.Message(root, text=cleanup(valid_text), background=alternate_colors, width='400')
+        clean_message.grid(column=1, row=loop_pos + 3, sticky='W' + 'E')
+        msg_counter.append(clean_message)
 
 # Set up window
 root = tk.Tk()
@@ -54,13 +87,12 @@ root.title("Twitter Cleaner")
 root.geometry("800x800")
 tk.Grid.columnconfigure(root, 0, weight=1)
 tk.Grid.columnconfigure(root, 1, weight=1)
-input_label = tk.Label(root, height=1, text="Search term or post URL").grid(column=0, row=0, sticky='W')
-user_input = tk.Entry(root, width=60)
-user_input.grid(column=0, row=1, sticky='W' + 'E')
+input_label = tk.Label(root, height=1, text="Search term or post URL").grid(column=0, row=0, sticky='W', padx=(15,0))
+user_input = tk.Entry(root)
+user_input.grid(column=0, row=1, sticky='W' + 'E', padx=(15, 15))
 button_run = tk.Button(root, text="Search", command=run_tweets).grid(column=1, row=1, sticky='W')
 user_input.bind('<Return>', run_tweets)
 orig_label = tk.Label(root, height=1, text="Original tweet", font='size=16').grid(column=0, row=2, sticky='W' + 'E')
-clean_label = tk.Label(root, height=1, text="Cleaned up tweet", font='size=16').grid(column=1, row=2, sticky='W'+'E')
-
+clean_label = tk.Label(root, height=1, text="Cleaned up tweet", font='size=16').grid(column=1, row=2, sticky='W' + 'E')
 
 root.mainloop()
